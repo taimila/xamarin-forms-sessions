67005a344e306049d2b495041736bdf7c06f2c22
Lauri Taimila <lauri.taimila@if.fi>
2018-01-23 15:00:52 +0200

    Code from first session

---
+++ WeatherExample/App.xaml.cs
@@ -0,0 +1,30 @@
+﻿using Xamarin.Forms;
+using System.Net.Http;
+
+namespace WeatherExample
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new WeatherPage(new HttpClient());
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
---
+++ WeatherExample/WeatherPage.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+using System.Net.Http;
+using Newtonsoft.Json.Linq;
+
+namespace WeatherExample
+{
+    public partial class WeatherPage : ContentPage
+    {
+        const string ApiKey = "3701bca2b823d836db615319af858afd";
+        readonly HttpClient httpClient;
+
+        // This is here for Preview to work
+        public WeatherPage()
+        {
+            InitializeComponent();
+        }
+
+        public WeatherPage(HttpClient httpClient)
+        {
+            this.httpClient = httpClient;
+            InitializeComponent();
+
+            MyBytton.BackgroundColor = Device.RuntimePlatform == Device.iOS ? Color.Yellow : Color.Blue;
+
+            City.Text = "Hesa";
+        }
+
+        protected override async void OnAppearing()
+        {
+            var location = "Turku";
+            var url = $"http://api.openweathermap.org/data/2.5/weather?q={location} &units=metric&appid={ApiKey}";
+
+            var json = await httpClient.GetStringAsync(url);
+            dynamic data = JObject.Parse(json);
+
+            var model = new Weather
+            {
+                City = data.name,
+                Temperature = data.main.temp,
+                Humidity = data.main.humidity,
+                Pressure = data.main.pressure,
+                MinTemperature = data.main.temp_min,
+                MaxTemperature = data.main.temp_max
+            };
+
+            BindingContext = model;
+            City.Text = model.City.ToUpper();
+
+        }
+
+        void Handle_Clicked(object sender, System.EventArgs e)
+        {
+            City.Text = "Turku";
+        }
+    }
+
+    class Weather     {         public string City { get; set; }         public string Temperature { get; set; }         public string Humidity { get; set; }         public string Pressure { get; set; }         public string MinTemperature { get; set; }         public string MaxTemperature { get; set; }     }
+}
